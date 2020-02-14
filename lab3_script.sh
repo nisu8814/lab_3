4fc6ae0688c833fa1bec3adeaff45e1d6d304b6e
nisu8814 <49689570+nisu8814@users.noreply.github.com>
2020-02-14 14:55:37 -0700

    Add files via upload
    commit

---
+++ lab3_script.sh
@@ -0,0 +1,23 @@
+# Authors: Nik Suyko
+# Date: 2/14/2020
+
+echo "Filename: "
+read fileName
+echo "RegEx: "
+read regEx
+
+grep -P "$regEx" "$fileName"
+
+grep -P -c "^[2-9]\d{2}-\d{3}-\d{4}$" regex_practice.txt
+
+echo "Email Count: "
+grep -c -e ".com" -e ".edu" regex_practice.txt
+
+echo "303 Numbers: "
+grep -P '303-' regex_practice.txt
+
+grep "@geocities.com" regex_practice.txt >> email_results.txt
+
+git add email_results.txt
+git commit "Email Results"
+
